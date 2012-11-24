a7d85861d6a866a626a77e8c45d4dade31495931
Pook <veeraya.pupat@gmail.com>
2012-11-24 13:19:58 +0800

    added rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ellen::Application.config.secret_token = 'b22d68c9f769b38347aed298a8ede641fd2ba52657196a738651d10717b7a7580ad99ef2c763fe8a6af56d3443cf191eb83f90e1c2bf907c38370111eca642fc'
