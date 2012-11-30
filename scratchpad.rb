6777a94d6fa57220001aa849e144c515a40263c7
Pook <veeraya.pupat@gmail.com>
2012-11-30 22:42:24 +0800

    more scratchpad code

--- scratchpad.rb
+++ scratchpad.rb
@@ -1,7 +1,23 @@
 t = Ticket.create(date: Date.today, status: "available")
 t = Ticket.create(date: "2012-11-25", status: "not available")
 
-t.ticketStatusChanges.create()
+# Populating test user database in heroku
+u1 = User.create(email: "veeraya.pupat@gmail.com", track_all: true)
+u2 = User.create(email: "veeraya.pupat+181920@gmail.com", track_all: false)
+u3 = User.create(email: "veeraya.pupat+27@gmail.com", track_all:false)
+
+t1 = Tracking.create(date: '2012-12-18')
+t2 = Tracking.create(date: '2012-12-19')
+t3 =Tracking.create(date: '2012-12-20')
+t4 =Tracking.create(date: '2012-12-21')
+t5 =Tracking.create(date: '2012-12-27')
+
+u1.trackings.push(t1)
+u1.trackings.push(t2)
+u1.trackings.push(t3)
+u2.trackings.push(t5)
+# end of test database code
+
 
 User.first.trackings.exists?(Tracking.first)
 
@@ -28,3 +44,5 @@ result = ActiveRecord::Base.connection.execute(query)
 result.each do |r|
   puts r["email"]
 end
+
+
