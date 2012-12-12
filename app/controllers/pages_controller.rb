41d17170c59a339fc6b8940d9c2e6f813ae22c46
Pook <veeraya.pupat@gmail.com>
2012-12-12 17:16:21 +0800

    avg ticket availability period calculation

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,6 +8,15 @@ class PagesController < ApplicationController
       @last_avail_hr_ago = (last_avail * 24).to_i
     end
 
+    total_avail_day = 0
+    from_avail = StatusChange.where("from_status = 'tickets-available'")
+    from_avail.each do |avail_end|
+      tic = avail_end.ticket  
+      avail_start = tic.statusChanges.where("to_status = 'tickets-available' AND created_at < \'#{avail_end.created_at.to_date}\'").order("created_at DESC").first
+      total_avail_day += avail_end.created_at.to_datetime - avail_start.created_at.to_datetime
+    end
+    @avg_avail_day, @avg_avail_hr = (total_avail_day/from_avail.length).divmod(1)
+    @avg_avail_hr = (@avg_avail_hr*24).to_i
 
 
   end
